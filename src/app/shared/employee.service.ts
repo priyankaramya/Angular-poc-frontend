bf52ead0b5ace8ebf39e0cac4b7df8f82065ca66
Priyanka Senthilkumar <priyanka.s@domain.rage-india.com>
2023-02-25 12:35:40 +0530

    poc

--- src/app/shared/employee.service.ts
+++ src/app/shared/employee.service.ts
@@ -5,11 +5,16 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Employee } from './employee.model';
 
+const headers = new HttpHeaders()
+  .set('content-type', 'application/json')
+  .set('Access-Control-Allow-Origin', '*')
+  .set('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+
 @Injectable()
 export class EmployeeService {
   selectedEmployee!: Employee;
   employees: Employee[] = [];
-  readonly baseURL = 'https://angular-poc-backend.vercel.app/employees';
+  readonly baseURL = ('https://angular-poc-backend.vercel.app/employees', { 'headers': headers });
 
   constructor(private http: HttpClient) { }
 
